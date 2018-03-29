cc47f3459e401c03d0f4d51697cdfce2d4dd6723
Rob Cobb <rwcobbjr@gmail.com>
2018-03-29 18:31:10 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,19 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :team
+  end
+
+  post '/team' do
+    <<-PAGE
+      Team Name: #{params[:name]}
+      Coach: #{params[:coach]}
+      Point Guard: #{params[:pg]}
+      Shooting Guard: #{params[:sg]}
+      Power Forward: #{params[:pf]}
+      Center: #{params[:c]}
+    PAGE
+  end
 
 end
